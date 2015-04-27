a8be1dd4b15115017dd6170ee79ba941bd711584
antoine <jcbantoine@gmail.com>
2015-04-27 13:57:20 +0200

    -ajout mapController

---
+++ WisApp/Controllers/MapController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WisApp.Controllers
+{
+    public class MapController : ApiController
+    {
+
+
+        [HttpGet]
+        public Map initMap()
+        {
+            var map = new Map();
+            var center = new Center();
+
+            center.latitude = 46.6698231;
+            center.longitude = 2.9012201;
+            map.center = center;
+            map.zoom = 6;
+
+            return map;
+        }
+    }
+}
