495619ccc62361c2780cd1e555fb06d13cacfd73
AngelOfFox <julien.bergaut@live.fr>
2015-04-28 12:25:50 +0200

    - push article fonctionnel

---
+++ WisApp/Scripts/Controller/push.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    var connection = $.hubConnection();
+    // Reference the proxy for the hub.  
+    var articleHubProxy = connection.createHubProxy('articleHub');
+    articleHubProxy.on('refresh', function () {
+        if (window.location == "http://localhost:52454/Content/index.html#/" || window.location == "http://localhost:52454/content/#/") {
+            window.location.reload();
+        }
+    })
+    
+    // Start the connection.
+    connection.start()
+        .done(function () {
+            $('#sendarticle').click(function () {
+                // Call the Send method on the hub.
+                articleHubProxy.invoke('refresh_server')
+            });
+    });
+});
